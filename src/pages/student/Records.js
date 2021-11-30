e4fd5802dd387a9f417af434e98e40d926c7d9db
ashish <ashish.goyal@nexxbase.com>
2021-11-30 13:54:47 +0530

    stage api student filter record change

--- src/pages/student/Records.js
+++ src/pages/student/Records.js
@@ -38,7 +38,7 @@ function Records(){
     let filterDate = async(e)=>{
         e.preventDefault()
         try {
-            let data = await axios.get(`${process.env.REACT_APP_BASE_URL}/student/records/${sDate}/${eDate}`)
+            let data = await axios.get(`${process.env.REACT_APP_BASE_URL}/student/records/filter/${sDate}/${eDate}`)
             console.log(data.data, "filter record data")
 
             setRecords(data.data.data)
